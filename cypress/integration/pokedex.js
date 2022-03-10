389866ea04a4a1f17976c5a51570b3a035ab3aed
luminouslily35 <97817095+luminouslily35@users.noreply.github.com>
2022-03-09 18:06:50 -0800

    cypress test case

--- cypress/integration/pokedex.js
+++ cypress/integration/pokedex.js
@@ -6,9 +6,18 @@
 // https://on.cypress.io/writing-first-test
 
 describe('Pokedex', function () {
-  it('front page can be opened', function () {
+  beforeEach(function () {
     cy.visit('http://localhost:5000')
+  })
+
+  it('front page can be opened', function () {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+
+  it('can access a specific pokemon', function () {
+    cy.contains('pichu').click()
+    cy.contains('static')
+    cy.contains('hp20')
   })
 })
